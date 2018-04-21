7635a7e4aad174f1d595133c11290c9bc9deced2
Muhammad Younas <MYounas@users.noreply.github.com>
2018-04-21 23:08:44 +0500

    pso graph added

--- PSO/main.py
+++ PSO/main.py
@@ -1,6 +1,10 @@
 import random, numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
 fit_func = lambda x, y: 100 * (x ** 2 - y) ** 2 + (1 - x) ** 2
+
+# random.seed(786)
 
 G = 10;
 n = 25;
@@ -14,6 +18,7 @@ for i in range(n):
     y = random.uniform(-1, 3)
     fit_val = fit_func(x, y)
     nList.append([x, y, fit_val])
+
 
 l_best = [];
 g_best = 0;
@@ -32,34 +37,51 @@ l_best.append((sorted(nList, key=lambda i: i[2], reverse=True))[0])
 
 g_best = (sorted(l_best, key=lambda i: i[2], reverse=True))[0]
 
-for i in range(G):
+fig=plt.figure()
 
-    print(g_best)
+ax1 = fig.add_subplot(111)
 
-    for ind, j in enumerate(nList):
-        # get x component of best of particle's whole iterations
-        p_best_x = p_best_list[ind][max(p_best_list[ind].keys())][0]
+def animate(i):
+    global g_best
 
-        velocities[ind]['x'] = velocities[ind]['x'] + c1 * random.uniform(0, 1) * (
-            p_best_x - j[0]) + c2 * random.uniform(0, 1) * (g_best[0] - j[0])
+    for i in range(G):
 
+        xs = [x for x in range(len(nList))]
+        ys = [x[2] for x in nList]
 
-        if j[0] + velocities[ind]['x'] >= -2 and j[0] + velocities[ind]['x'] <= 2:
-            j[0] += velocities[ind]['x']
+        print(g_best)
 
-        p_best_y = p_best_list[ind][max(p_best_list[ind].keys())][1]
+        for ind, j in enumerate(nList):
+            # get x component of best of particle's whole iterations
+            p_best_x = p_best_list[ind][max(p_best_list[ind].keys())][0]
 
-        velocities[ind]['y'] = velocities[ind]['y'] + c1 * random.uniform(0, 1) * (
-            p_best_y - j[1]) + c2 * random.uniform(0, 1) * (g_best[1] - j[1])
-
-        if j[1] + velocities[ind]['y'] >= -1 and j[1] + velocities[ind]['y'] <= 3:
-            j[1] += velocities[ind]['y']
-
-        j[2] = fit_func(j[0], j[1])
-
-        p_best_list[ind][j[2]] = j
+            velocities[ind]['x'] = velocities[ind]['x'] + c1 * random.uniform(0, 1) * (
+                p_best_x - j[0]) + c2 * random.uniform(0, 1) * (g_best[0] - j[0])
 
 
-    l_best.append((sorted(nList, key=lambda i: i[2], reverse=True))[0])
+            if j[0] + velocities[ind]['x'] >= -2 and j[0] + velocities[ind]['x'] <= 2:
+                j[0] += velocities[ind]['x']
 
-    g_best = (sorted(l_best, key=lambda i: i[2], reverse=True))[0]
+            p_best_y = p_best_list[ind][max(p_best_list[ind].keys())][1]
+
+            velocities[ind]['y'] = velocities[ind]['y'] + c1 * random.uniform(0, 1) * (
+                p_best_y - j[1]) + c2 * random.uniform(0, 1) * (g_best[1] - j[1])
+
+            if j[1] + velocities[ind]['y'] >= -1 and j[1] + velocities[ind]['y'] <= 3:
+                j[1] += velocities[ind]['y']
+
+            j[2] = fit_func(j[0], j[1])
+
+            p_best_list[ind][j[2]] = j
+
+
+        l_best.append((sorted(nList, key=lambda i: i[2], reverse=True))[0])
+
+        g_best = (sorted(l_best, key=lambda i: i[2], reverse=True))[0]
+
+
+    ax1.clear()
+    ax1.scatter(xs, ys)
+
+ani=animation.FuncAnimation(fig,animate,interval=100)
+plt.show()

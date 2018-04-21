7e7105afc280d0de76b3f1847438fa6c26d090fc
Muhammad Younas <MYounas@users.noreply.github.com>
2018-04-21 08:43:08 +0500

    done

--- ANN/annalgoUpdated.py
+++ ANN/annalgoUpdated.py
@@ -10,13 +10,15 @@ data = df.values.tolist()
 #normalization
 data = list(map(lambda i: list(map(lambda j: j / 20000, i)), data))
 
-#break data in training & testing
-train_data=data[:500]
-test_data=data[500:]
+# #break data in training & testing
+# train_data=data[:500]
+# test_data=data[500:]
 
 # outer_threshold = 0.001;
 inner_threshold = 0.005;
 rate_var = 0;
+
+final_ansr=[]
 
 # floating random weights
 w14, w15, w24, w25, w34, w35, w46, w56 = np.random.uniform(-1, 1, 8)
@@ -28,20 +30,20 @@ while (1):
     rec_index = 0
     # learning rate
     n = 1 / (len(data) + rate_var)
-    print("rate_var:{}".format(rate_var))
+    print("gen:{}".format(rate_var))
     rate_var += 1
 
     #randomly get records,use this only if not first iteration and you not want to check threshold on avg
     # data=random.sample(data,len(data))
 
-    for i in train_data:
+    for i in data:
         rec_index += 1
-        print("record:{}".format(i))
+        # print("record:{}".format(i))
         x1, x2, x3, y = i[0], i[1], i[2], i[3]
 
         while (1):
-            print("record index:{}".format(rec_index))
-            print("rate_var:{}".format(rate_var))
+            # print("record index:{}".format(rec_index))
+            # print("rate_var:{}".format(rate_var))
             out4 = (x1 * w14) + (x2 * w24) + (x3 * w34) + o4
             out4 = 1 / (1 + math.e ** (-out4))
 
@@ -53,7 +55,7 @@ while (1):
 
             err6 = out6 * (1 - out6) * (y - out6)
 
-            if err6 <= 0.005:
+            if abs(err6) <= inner_threshold:
                 # avg_total.append(err6)
                 break
 
@@ -98,7 +100,7 @@ while (1):
 
     test=0
 
-    for i in test_data:
+    for i in data:
         x1, x2, x3, y = i[0], i[1], i[2], i[3]
 
         out4 = (x1 * w14) + (x2 * w24) + (x3 * w34) + o4
@@ -112,7 +114,8 @@ while (1):
 
         err6 = out6 * (1 - out6) * (y - out6)
 
-        if err6 <= inner_threshold:
+        if abs(err6) <= inner_threshold:
+            # final_ansr.append(out6 * 20000)
             test=1
         else:
             test = 0
@@ -120,3 +123,33 @@ while (1):
 
     if test:
         break
+
+    if rate_var > 300:
+        break
+
+print("\nFinal Weights and biases\n")
+print(f'w14:{w14}, w15:{w15}, w24:{w24}, w25:{w25}, w34:{w34}, w35:{w35}, w46:{w46}, w56:{w56},o4:{o4}, o5:{o5}, o6:{o6}')
+
+#writing data on the basis of fittest
+for i in data:
+
+    x1, x2, x3, y = i[0], i[1], i[2], i[3]
+
+    out4 = (x1 * w14) + (x2 * w24) + (x3 * w34) + o4
+    out4 = 1 / (1 + math.e ** (-out4))
+
+    out5 = (x1 * w15) + (x2 * w25) + (x3 * w35) + o5
+    out5 = 1 / (1 + math.e ** (-out5))
+
+    out6 = (out4 * w46) + (out5 * w56) + o6
+    out6 = 1 / (1 + math.e ** (-out6))
+
+    err6 = out6 * (1 - out6) * (y - out6)
+
+    final_ansr.append(out6 * 20000)
+
+
+final_ansr=final_ansr[-len(data):]
+
+df['Y-ANN']=pd.DataFrame(final_ansr)
+df.to_csv('NN-DATA2.csv',index=False)
--- neuro-evolution/main.py
+++ neuro-evolution/main.py
@@ -2,6 +2,9 @@ import pandas as pd
 import numpy as np
 import random, math
 
+random.seed(786)
+np.random.seed(786)
+
 df = pd.read_csv('NN-DATA.csv')
 
 # convert data to list of lists
@@ -10,9 +13,10 @@ data = df.values.tolist()
 # normalization
 data = list(map(lambda i: list(map(lambda j: j / 20000, i)), data))
 
-
 inner_threshold = 0.005;
 rate_var = 0;
+
+final_ansr = []
 
 
 def fit_func(rec, i):
@@ -29,7 +33,8 @@ def fit_func(rec, i):
 
     err6 = out6 * (1 - out6) * (y - out6)
 
-    if err6 <= inner_threshold:
+    if abs(err6) <= inner_threshold:
+        # final_ansr.append(out6 * 20000)
         rec['fitness'] += 1
 
 
@@ -38,33 +43,51 @@ m = 20;
 n = 25;
 mutation_prob = 75;
 mutation = 0.5;
+
+# ranges for weights & biases
+wl = -2;
+wr = 2;
+bl = -1;
+br = 1
+
 for i in range(n):
     # floating random weights
-    w14, w15, w24, w25, w34, w35, w46, w56 = np.random.uniform(-1, 1, 8)
+    w14, w15, w24, w25, w34, w35, w46, w56 = np.random.uniform(wl, wr, 8)
     # floating random biases
-    o4, o5, o6 = np.random.uniform(0, 1, 3)
+    o4, o5, o6 = np.random.uniform(bl, br, 3)
+
     nList.append({'data': {'w14': w14, 'w15': w15, 'w24': w24, 'w25': w25, 'w34': w34, 'w35': w35, 'w46': w46,
                            'w56': w56, 'o4': o4, 'o5': o5, 'o6': o6}, 'fitness': 0})
 
-#fitness calculation for first time/initial population
+# fitness calculation for first time/initial population
 for rec in nList:
     for i in data:
         fit_func(rec, i)
 
-count = 0
-flag=False
+count = 0;
+final_rec = 0
+flag = False
+stop_at=600
 while (1):
     count += 1
     mList = []
 
-    rec_num=0
+    print(f"gen: {count}")
+
+    if count > stop_at:
+        final_rec = sorted(nList, key=lambda f: f['fitness'], reverse=True)[0]
+        break
+
+    rec_num = 0
     for rec in nList:
-        print("rec_index: {}".format(rec_num))
-        rec_num+=1
+        # print("rec_index: {}".format(rec_num))
+        rec_num += 1
         if rec['fitness'] == len(data):
-            print('gen: {}'.format(count))
+            print("rec_index: {}".format(rec_num))
+            final_rec = rec
+            # print('gen: {}'.format(count))
             print('Answer: {}'.format(rec))
-            flag=True
+            flag = True
             break
 
     if flag:
@@ -93,33 +116,33 @@ while (1):
                 # odd,do +ve
                 if random.randint(1, 2) == 1:
                     # mutation in weights
-                    if (k[t[0]] + mutation) >= -1 and (k[t[0]] + mutation) <= 1:
+                    if (k[t[0]] + mutation) >= wl and (k[t[0]] + mutation) <= wr:
                         k[t[0]] += mutation
 
-                    if (k[t[1]] + mutation) >= -1 and (k[t[1]] + mutation) <= 1:
+                    if (k[t[1]] + mutation) >= wl and (k[t[1]] + mutation) <= wr:
                         k[t[1]] += mutation
 
-                    if (k[t[2]] + mutation) >= -1 and (k[t[2]] + mutation) <= 1:
+                    if (k[t[2]] + mutation) >= wl and (k[t[2]] + mutation) <= wr:
                         k[t[2]] += mutation
 
                     # mutation in bias
-                    if (k[t1] + mutation) >= 0 and (k[t1] + mutation) <= 1:
+                    if (k[t1] + mutation) >= bl and (k[t1] + mutation) <= br:
                         k[t1] += mutation
 
                 # even,do -ve
                 else:
                     # mutation in weights
-                    if (k[t[0]] - mutation) >= -1 and (k[t[0]] - mutation) <= 1:
+                    if (k[t[0]] - mutation) >= wl and (k[t[0]] - mutation) <= wr:
                         k[t[0]] -= mutation
 
-                    if (k[t[1]] - mutation) >= -1 and (k[t[1]] - mutation) <= 1:
+                    if (k[t[1]] - mutation) >= wl and (k[t[1]] - mutation) <= wr:
                         k[t[1]] -= mutation
 
-                    if (k[t[2]] - mutation) >= -1 and (k[t[2]] - mutation) <= 1:
+                    if (k[t[2]] - mutation) >= wl and (k[t[2]] - mutation) <= wr:
                         k[t[2]] -= mutation
 
                     # mutation in bias
-                    if (k[t1] - mutation) >= 0 and (k[t1] - mutation) <= 1:
+                    if (k[t1] - mutation) >= bl and (k[t1] - mutation) <= br:
                         k[t1] -= mutation
 
         ch1 = {'data': {'w14': ch1[0], 'w15': ch1[1], 'w24': ch1[2], 'w25': ch1[3], 'w34': ch1[4], 'w35': ch1[5],
@@ -130,15 +153,37 @@ while (1):
                         'w46': ch2[6],
                         'w56': ch2[7], 'o4': ch2[8], 'o5': ch2[9], 'o6': ch2[10]}, 'fitness': 0}
 
-        #calculate fitness of children
-        for rec in [ch1,ch2]:
+        # calculate fitness of children
+        for rec in [ch1, ch2]:
             for i in data:
                 fit_func(rec, i)
 
         mList.append(ch1)
         mList.append(ch2)
 
-
     # combine both mList and nList and sort it with respect fit func value,also ovverides the nList,so we used in next iteration
 
     nList = sorted(nList + mList, key=lambda item: item['fitness'], reverse=True)[:25]
+
+# writing data on the basis of fittest
+for i in data:
+    x1, x2, x3, y = i[0], i[1], i[2], i[3]
+
+    out4 = (x1 * final_rec['data']['w14']) + (x2 * final_rec['data']['w24']) + (x3 * final_rec['data']['w34']) + \
+           final_rec['data']['o4']
+    out4 = 1 / (1 + math.e ** (-out4))
+
+    out5 = (x1 * final_rec['data']['w15']) + (x2 * final_rec['data']['w25']) + (x3 * final_rec['data']['w35']) + \
+           final_rec['data']['o5']
+    out5 = 1 / (1 + math.e ** (-out5))
+
+    out6 = (out4 * final_rec['data']['w46']) + (out5 * final_rec['data']['w56']) + final_rec['data']['o6']
+    out6 = 1 / (1 + math.e ** (-out6))
+
+    err6 = out6 * (1 - out6) * (y - out6)
+
+    final_ansr.append(out6 * 20000)
+
+# final_ansr=final_ansr[-len(data):]
+df['Y-ENN'] = pd.DataFrame(final_ansr)
+df.to_csv('NN-DATA2.csv', index=False)
